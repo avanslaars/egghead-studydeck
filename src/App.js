7fbe0e1c95253806d17db7bcfeaa40d4cc001121
Andrew Van Slaars <avanslaars@gmail.com>
2019-12-06 18:26:53 -0500

    added isFront state with useState

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ function App() {
         <main>
           <h3>Your Cards</h3>
           <div className="gridContainer">
-            <CardPreview term="What does a duck say?" />
+            <CardPreview definition="quack" term="What does a duck say?" />
+            <CardPreview definition="woof" term="What does a dog say?" />
+            <CardPreview definition="moo" term="What does a cow say?" />
           </div>
         </main>
       </div>
--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export function CardPreview(props) {
+  const [isFront, setIsFront] = React.useState(true)
+  function handleCardFlip() {
+    setIsFront(current => !current)
+  }
   return (
-    <div className="tile">
-      <h4 className="cardTerm">{props.term}</h4>
+    <div className={`tile ${isFront ? '' : 'back'}`}>
+      <h4 className="cardTerm">{isFront ? props.term : props.definition}</h4>
       <div className="cardButtons">
-        <button type="button" className="tertiary">
-          show back
+        <button type="button" className="tertiary" onClick={handleCardFlip}>
+          {isFront ? 'show back' : 'show front'}
         </button>
         <div>
           <button type="button" className="secondary">
