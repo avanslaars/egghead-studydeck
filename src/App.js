ccaebd712d24e698a3fbfdf89ce5db81bd8407ab
Andrew Van Slaars <avanslaars@gmail.com>
2019-12-06 18:06:47 -0500

    static JSX added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div>Hello, world!</div>
+  return (
+    <div>
+      <div>
+        <header>
+          <h1>StudyDeck</h1>
+          <h2>Retention through repitition</h2>
+        </header>
+        <main>
+          <h3>Your Cards</h3>
+          <div>
+            <div>
+              <h4>Term goes here</h4>
+              <div>
+                <button type="button">show back</button>
+                <button type="button">edit</button>
+                <button type="button">delete</button>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  )
 }
 
 export default App
