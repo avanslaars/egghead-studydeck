4a30616e8f78d11d7332ca278e73c12e053d220b
Andrew Van Slaars <avanslaars@gmail.com>
2019-12-07 12:54:43 -0500

    mapping over cards array

--- src/App.js
+++ src/App.js
@@ -21,11 +21,10 @@ function App() {
         </header>
         <main>
           <h3>Your Cards</h3>
-          <pre>{JSON.stringify(cards, null, 2)}</pre>
           <div className="gridContainer">
-            <CardPreview definition="quack" term="What does a duck say?" />
-            <CardPreview definition="woof" term="What does a dog say?" />
-            <CardPreview definition="moo" term="What does a cow say?" />
+            {cards.map(({ id, definition, term }) => (
+              <CardPreview key={id} definition={definition} term={term} />
+            ))}
           </div>
         </main>
       </div>
