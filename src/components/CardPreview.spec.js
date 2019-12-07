1eba517473189615954105ef7fd45b0c707b17ee
Andrew Van Slaars <avanslaars@gmail.com>
2019-12-07 11:40:57 -0500

    added click event test

--- src/components/CardPreview.spec.js
+++ src/components/CardPreview.spec.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { CardPreview } from './CardPreview'
 
 it('Renders the expected term', () => {
   const expectedTerm = 'this is a test'
   const { getByText } = render(<CardPreview term={expectedTerm} />)
   expect(getByText(expectedTerm)).toBeInTheDocument()
+})
+
+it('Flips the card to reveal the definition', () => {
+  const expectedTerm = 'This is the term'
+  const expectedDef = 'This is the definition'
+  const { getByText, queryByText } = render(
+    <CardPreview term={expectedTerm} definition={expectedDef} />
+  )
+
+  expect(getByText(expectedTerm)).toBeInTheDocument()
+
+  const flipButton = getByText(/show/i)
+  fireEvent.click(flipButton)
+
+  expect(getByText(expectedDef)).toBeInTheDocument()
+  expect(queryByText(expectedTerm)).not.toBeInTheDocument()
 })
