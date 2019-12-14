3a90f17546d894c3fdb0cfe2ade0096fd3b75d5c
Andrew Van Slaars <avanslaars@gmail.com>
2019-12-14 11:19:00 -0500

    convert practice to a class component

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
-export function Practice() {
-  return <div>practice here</div>
+export class Practice extends React.Component {
+  render() {
+    return (
+      <div className="practiceContainer">
+        <h3>Practice</h3>
+        <div className="progress">1/5</div>
+        <div className="card">
+          <div className="practiceTerm">What sound does a duck make?</div>
+          <div className="practiceButtons">
+            <button type="button" className="tertiary">
+              show back
+            </button>
+            <div>
+              <button type="button" className="secondary">
+                previous
+              </button>
+              <button type="button" className="primary">
+                next
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
